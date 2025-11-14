6e65cf5be6415a635060350c4535704a436f33fb
IvSan <foritun@gmail.com>
2025-11-14 21:41:04 +0100

    adafruit test

---
+++ adafruit.py
@@ -0,0 +1,11 @@
+import board
+import digitalio
+import adafruit_max31856
+spi = board.SPI()
+cs = digitalio.DigitalInOut(board.D5)
+cs.direction = digitalio.Direction.OUTPUT
+thermocouple = adafruit_max31856.MAX31856(spi,cs)
+
+while True:
+    print(thermocouple.temperature)
+    time.sleep(1)
