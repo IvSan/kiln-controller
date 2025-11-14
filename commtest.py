2c39a865296ebbd9e5a46daf12d149d66d9b4ec3
IvSan <foritun@gmail.com>
2025-11-14 18:31:57 +0100

    comm test

---
+++ commtest.py
@@ -0,0 +1,6 @@
+import spidev
+spi = spidev.SpiDev()
+spi.open(0, 0)  # bus 0, device 0 (adjust if using CE1)
+spi.max_speed_hz = 500000
+resp = spi.xfer2([0x00, 0x00, 0x00, 0x00])  # send dummy bytes, expect response
+print(resp)
