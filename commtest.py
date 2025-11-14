955ea6d7ee9c3919b4ac348ce200941e5977425b
IvSan <foritun@gmail.com>
2025-11-14 18:37:34 +0100

    comm test

--- commtest.py
+++ commtest.py
@@ -7,3 +7,6 @@ spi.open(0, 0)  # bus 0, device 0 (adjust if using CE1)
 spi.max_speed_hz = 500000
 resp = spi.xfer2([0x00, 0x00, 0x00, 0x00])  # send dummy bytes, expect response
 print(resp)
+
+resp = spi.xfer2([0x0F | 0x80]) + spi.xfer2([0x00])  # Read command + read response
+print(resp)
