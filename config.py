2fc21dac32d84d21fb756ceb6fa3ed801e12dc23
IvSan <foritun@gmail.com>
2025-11-22 22:53:19 +0100

    Throttling

--- config.py
+++ config.py
@@ -278,5 +278,5 @@ kiln_profiles_directory = os.path.abspath(os.path.join(os.path.dirname( __file__
 # control window and below throttle_below_temp, only throttle_percent
 # of the elements are used max.
 # To prevent throttling, set throttle_percent to 100.
-throttle_below_temp = 200
+throttle_below_temp = 1
 throttle_percent = 100
